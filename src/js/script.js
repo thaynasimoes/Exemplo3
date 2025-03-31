67ca6bf8a816c0aa0d014ec840e1d37fbe21c3d0
Thayna <thayna.pereira2402@gmail.com>
2025-03-31 09:29:03 -0300

    Conhecendo alguns metodos

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,17 @@ console.log(parseInt( numFloat));
 
 let numString = "123.456";
 console.log(parseFloat(numString));
+
+//Metodos
+
+//retorna o numero de caracteres
+let frase = "O mundo da tecnologia";
+console.log(frase.length);
+
+//retorna a posicao de um trecho do texto
+let texto = "A programacão indomavel e sustentavel";
+console.log(texto.indexOf("ão"));
+
+//retorna o trecho de texto que esta entre as posicoes
+let info = "Processamento de ponta";
+console.log(info.slice(16,25));
